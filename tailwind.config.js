ada7685bd8e694375c0cf7b075dc5495d9e03131
jenerCruz <pamecoaxaca.telcdl@gmail.com>
2025-12-16 18:56:57 -0600

    bb

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  presets: [require("./ui/tailwind.config.js")],
-  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}", "!./node_modules/**/*"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
